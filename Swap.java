ed81984480150d09b2593268003132fef07fcfbf
Elisabeth <elisabeth.seite@gmail.com>
2017-04-18 21:38:00 -0400

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -18,7 +18,7 @@ class Swap
       			//check if the value needs to go right
       			if (value % 2 == 0)
       			{
-        			//check value to the left
+        			//check value to the right
         			for (int a = i; a < array.length; a++)
         			{
           				int next = array[a];
@@ -43,7 +43,7 @@ class Swap
       			// or if the value should go left
       			else
 			{
-        			//check value to the right
+        			//check value to the left
         			for (int a = i; a >= 0; a--)
         			{
          				int next = array[a];
