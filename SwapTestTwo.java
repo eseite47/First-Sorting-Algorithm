25d7886c0e299ea3f17a4f56e5eadae92c79d95f
Elisabeth <elisabeth.seite@gmail.com>
2017-04-21 16:01:22 -0400

    Create SwapTestTwo.java
    Test program for second approach that used separate arrays.

---
+++ SwapTestTwo.java
@@ -0,0 +1,9 @@
+class SwapTestTwo
+{
+	public static void main (String[] args)
+	{
+		SwapTwo test = new SwapTwo (new int[] {8, 5 , 6, 4, 3, 2, 7, 1, 9});
+		test.sortItOut();
+		test.printList();
+	}
+}
